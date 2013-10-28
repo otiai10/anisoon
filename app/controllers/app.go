f190aa6ce79d5257668ceb983a5b4ba5247c38b5
otiai10 <otiai10@gmail.com>
2013-10-28 11:07:56 +0900

    Remove unnecessary pkg : sugoi

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
   "github.com/robfig/revel"
-  "github.com/r7kamura/sugoi"
-  "fmt"
 )
 
 type App struct {
@@ -11,7 +9,5 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-  client := sugoi.NewClient()
-  client.GetTitles("updatedFrom", "2000-01-01T00:00:00+09:00")
 	return c.Render()
 }
