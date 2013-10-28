0a9d6ce39ad77e16029b397aa69a1babdac52974
otiai10 <otiai10@gmail.com>
2013-10-28 22:48:22 +0900

    Connect to YouTube Player API

--- client/src/lab.ts
+++ client/src/lab.ts
@@ -8,42 +8,89 @@
 /// <reference path="../external/underscore.d.ts" />
 /// <reference path="../external/backbone.d.ts" />
 
+var swfobject = swfobject || {};
 var anisonList = anisonList || [];
 
-module Anison {
+var onYouTubePlayerReady = (playerId) => {
+    this.player = document.getElementById("player");
+    this.player.playVideo();
+    this.isPlayerReady = true;
+}
+
+module Anisoon {
     export class Lab {
+
+        public isPlayerReady: boolean = false;
+        public player: any;
+
         constructor(){
-            var Anison = Backbone.Model.extend({});
-            var Anisons = Backbone.Collection.extend({model: Anison});
-            var AnisonView = Backbone.View.extend({
-                tagName: 'tr',
-                className: 'table',
-                template: _.template($("#anison-tpl").html()),
-                render: function(): Backbone.View {
-                    var tpl = this.template(this.model.toJSON());
-                    this.$el.html(tpl);
-                    return this;
-                }
-            })
-            var AnisonsView = Backbone.View.extend({
+            var anisons = new Anisoon.Anisons(anisonList);
+            var anisonsView = new Anisoon.AnisonsView({
+                collection : anisons,
                 tagName: 'table',
-                render : function(): Backbone.View {
-                    this.collection.each(anison => {
-                        var anisonView = new AnisonView({model:anison});
-                        this.$el.append(anisonView.render().$el);
-                    });
-                    return this;
-                }
-            });
-            var anisons = new Anisons(anisonList);
-            var anisonsView = new AnisonsView({
-                collection : anisons
+                className: 'table'
             });
             $("#anison-container").html(anisonsView.render().el);
+
+            // ためしに
+            setTimeout(() => {
+                this.play("rvhbnKaX3p8");
+            },3000);
+        }
+
+        play(vhash: string) {
+            if (this.isPlayerReady) {
+                this.player.loadVideoById(vhash);//ByUrlの方がいいかな？
+                return;
+            }
+            swfobject.embedSWF(
+                "http://www.youtube.com/v/"+ vhash +"?enablejsapi=1&playerapiid=player",//Initial URL
+                "player",// DOM id
+                "400",// width
+                "300",// height
+                "8",// SWF Version
+                null,null,
+                {allowScriptAccess: "always"},
+                {id:"player"}
+            );
+
+        }
+    }
+
+    export class Anison extends Backbone.Model {
+    }
+    export class Anisons extends Backbone.Collection {
+        public model: Anison;
+    }
+    export class AnisonsView extends Backbone.View {
+        constructor(options?){
+            super(options);
+        }
+        render(): AnisonsView {
+            this.collection.each(anison => {
+                var anisonView = new AnisonView({
+                    model:anison,
+                    tagName: 'tr'
+                });
+                this.$el.append(anisonView.render().$el);
+            });
+            return this;
+        }
+    }
+
+    export class AnisonView extends Backbone.View {
+        constructor(options?){
+            super(options);
+        }
+        public template = _.template($("#anison-tpl").html());
+        render(): AnisonView {
+            var tpl = this.template(this.model.toJSON());
+            this.$el.html(tpl);
+            return this;
         }
     }
 }
 
 $(function(){
-    var anisonLab = new Anison.Lab();
+    var anisonLab = new Anisoon.Lab();
 });
