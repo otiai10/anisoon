3f60ab2719753c122942cbf4a7e66b832e999975
Hiromu OCHIAI <otiai10@gmail.com>
2022-08-19 17:27:16 +0900

    fmt

--- models/anisoon/index.ts
+++ models/anisoon/index.ts
@@ -1,42 +1,42 @@
 // @deno-type="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/google-apps-script/apis/youtube_v3.d.ts"
 
-import { Cast, Staff, Song as SyobocalSong } from "../syobocal/syobocaldb.ts";
+import { Cast, Song as SyobocalSong, Staff } from "../syobocal/syobocaldb.ts";
 
 export interface Anime {
-    tid: number;
-    title: string;
-    last_update: string;
-    episodes?: string[]; // FIXME: 自信無い
-    staff: Staff;
-    cast: Cast;
+  tid: number;
+  title: string;
+  last_update: string;
+  episodes?: string[]; // FIXME: 自信無い
+  staff: Staff;
+  cast: Cast;
 }
 
 export interface Song extends SyobocalSong {}
 
 export interface SearchItem {
-    anime: Anime;
-    song: Song;
+  anime: Anime;
+  song: Song;
 }
 
 export interface Track {
-    anime: Anime;
-    song:  Song;
-    youtube: {
-        query: {
-            keyword: string;
-            params: string;
-            found: number;
-        };
-        videos: {
-            id: string;
-            kind: string;
-            title: string;
-            publishedAt: string;
-            channel: {
-                id: string;
-                title: string;
-            };
-            thumbnails: GoogleAppsScript.YouTube.Schema.ThumbnailDetails,
-        }[];
+  anime: Anime;
+  song: Song;
+  youtube: {
+    query: {
+      keyword: string;
+      params: string;
+      found: number;
     };
+    videos: {
+      id: string;
+      kind: string;
+      title: string;
+      publishedAt: string;
+      channel: {
+        id: string;
+        title: string;
+      };
+      thumbnails: GoogleAppsScript.YouTube.Schema.ThumbnailDetails;
+    }[];
+  };
 }
--- models/syobocal/syobocaldb.ts
+++ models/syobocal/syobocaldb.ts
@@ -1,30 +1,29 @@
-
 export interface SyobocalJSONDBEntry {
-    context: {
-        time: string;
-        query: string;
-        output: string;
-    };
-    animes: Anime[];
+  context: {
+    time: string;
+    query: string;
+    output: string;
+  };
+  animes: Anime[];
 }
 
 export interface Anime {
-    tid: number;
-    title: string;
-    last_update: string;
-    episodes?: string[]; // FIXME: 自信無い
-    songs?: Song[];
-    staff: Staff;
-    cast: Cast;
+  tid: number;
+  title: string;
+  last_update: string;
+  episodes?: string[]; // FIXME: 自信無い
+  songs?: Song[];
+  staff: Staff;
+  cast: Cast;
 }
 
 export interface Song {
-    title: string;
-    label: string;
-    words?: string[];
-    music?: string[];
-    composer?: string[];
-    singer?: string[];
+  title: string;
+  label: string;
+  words?: string[];
+  music?: string[];
+  composer?: string[];
+  singer?: string[];
 }
 
 export type Staff = { [role: string]: string[] };
