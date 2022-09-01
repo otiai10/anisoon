f243e5915f6bddf593a5a9136405a6f83aad5977
Hiromu OCHIAI <otiai10@gmail.com>
2022-09-01 18:38:12 +0900

    DBの更新をanimegeekに任せる

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Deploy](https://github.com/otiai10/anisoon/actions/workflows/deploy.yml/badge.svg)](https://github.com/otiai10/anisoon/actions/workflows/deploy.yml)
-[![Daily Update](https://github.com/otiai10/anisoon/actions/workflows/daily-update.yml/badge.svg)](https://github.com/otiai10/anisoon/actions/workflows/daily-update.yml)
+
+[![Daily Update](https://github.com/animegeek/anisoondb/actions/workflows/daily-update.yml/badge.svg)](https://github.com/animegeek/anisoondb/actions/workflows/daily-update.yml)
--- routes/api/tracks/latest.ts
+++ routes/api/tracks/latest.ts
@@ -1,11 +1,12 @@
 import { HandlerContext } from "$fresh/server.ts";
 
 const DATABASE_URL =
-  `https://raw.githubusercontent.com/otiai10/anisoon/main/db`;
+  `https://raw.githubusercontent.com/animegeek/anisoondb/main/db`;
 export const handler = async (
   _req: Request,
   _ctx: HandlerContext,
 ): Promise<Response> => {
+  console.log(DATABASE_URL);
   const [latest, ..._] =
     await (await fetch(`${DATABASE_URL}/tracks/index.json`)).json();
   const res = await fetch(`${DATABASE_URL}/tracks${latest.path}`);
